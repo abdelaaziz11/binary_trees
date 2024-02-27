184c702c56731f5a2cde4b90ce9a40738a0b2ee9
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-02-27 08:01:07 -0800

    insert left child

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,13 +11,14 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *new_node;
 
 	new_node = malloc(sizeof(binary_tree_t));
-	if (!parent ||	!new_node)
+	if (!parent)
 		return (NULL);
 	new_node->parent = parent;
 	new_node->n = value;
 	new_node->left = NULL;
 	new_node->right = NULL;
-
+	if (!new_node)
+                return (NULL);
 	if (parent->left)
 	{
 		new_node->left = parent->left;
