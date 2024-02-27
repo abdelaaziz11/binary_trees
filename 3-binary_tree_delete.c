66c74ece1ba343c50f3f23b1826b167e57829e9b
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-02-27 12:49:32 -0800

    delete tree

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -11,14 +11,7 @@ void binary_tree_delete(binary_tree_t *tree)
 {
 	if (!tree)
 		return;
-	else
-	{
-	if (tree)
-	{
 	binary_tree_delete(tree->right);
 	binary_tree_delete(tree->left);
-	}
 	free(tree);
-
-	}
 }
