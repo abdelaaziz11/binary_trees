920fcf8320d2f7eb9b06b5c52317b7a53d42ee1f
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-02-28 06:40:44 -0800

    size binary tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -7,9 +7,9 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t m = 0;
-
 	if (!tree)
-		return (NULL);
+		return (0);
 
+
+	return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
 }
