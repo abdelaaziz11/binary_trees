46d111387062591d0a8db36ecdc405609b72e6ee
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-02-28 15:34:03 -0800

    sibling binary tree

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -9,8 +9,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (!node ||	!node->parent)
 		return (NULL);
-	if ( node->parent->left == node)
+	if (node->parent->left == node)
 		return (node->parent->right);
-	else
-		return (node->parent->right);
+	return (node->parent->right);
 }
