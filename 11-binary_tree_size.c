b7ae764c5a15f9ce08ead055ce0b6fc8395ad76e
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-02-28 05:50:22 -0800

    measures size

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - measures the size ofbinary tree
+ * @tree: node of measures the size
+ * Return: 0 if NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t m = 0;
+
+	if (!tree)
+		return (NULL);
+
+}
