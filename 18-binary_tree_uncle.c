58a98053fe97eff0721d09e6ca9214d84b68743b
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-02-28 16:21:59 -0800

    uncle binary tree

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,7 +9,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	binary_tree_t *Gparent = NULL, *parent = NULL;
 
-	if (!node ||	!node->parent->parent ||	!node->parent)
+	if (!node ||	!node->parent ||	!node->parent->parent)
 		return (NULL);
 
 	parent = node->parent;
